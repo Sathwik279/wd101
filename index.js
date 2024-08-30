48996910e263fab5e0d4412e9cf4e800daaf946c
Sathwik279 <127001952+Sathwik279@users.noreply.github.com>
2024-08-30 09:10:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,61 @@
-let userForm = document.getElementById("user-form");
-const retrieveEntries = ()=>{
+let userForm = document.getElementById("user-form");//accessing the form element from html
+
+
+//retrieve data from local storage
+const retrieveEntries = () => {
     let entries = localStorage.getItem("user-entries");
-    if(entries){
-        entries = JSON.parse(entries);
-    }
-    else{
-        entries = [];
+    if (entries) {
+        entries = JSON.parse(entries);//if it is there in ls as a string parse it into an object
+    } else {
+        entries = []; //if not there return an empty array
     }
     return entries;
-}
-let userEntries = retrieveEntries();
-
-const displayEntries = ()=>{
-    const entries = retrieveEntries();
+};
 
 
-   const tableEntries  =  entries.map((entry)=>{
-        const nameCell = `<td class='border px-4 py-2'>${entry.name}</td>`
-        const emailCell = `<td class='border px-4 py-2'>${entry.email}</td>`
-        const passwordCell = `<td class='border px-4 py-2'>${entry.password}</td>`
-        const dobCell = `<td class='border px-4 py-2'>${entry.dob}</td>`
-        const acceptTermsCell = `<td class='border px-4 py-2'>${entry.acceptedTermsAndconditions}</td>`
-        
-        const row = `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell}  ${acceptTermsCell}`
+//this displays the data in the table after accessing from local storage
+const displayEntries = () => {
+    const entries = retrieveEntries();//returns a parsed object
+    // (console.logentries);
 
+    const tableEntries = entries.map((entry) => {
+        const nameCell = `<td class='border px-4 py-2'>${entry.name}</td>`;
+        const emailCell = `<td class='border px-4 py-2'>${entry.email}</td>`;
+        const passwordCell = `<td class='border px-4 py-2'>${entry.password}</td>`;
+        const dobCell = `<td class='border px-4 py-2'>${entry.dob}</td>`;
+        const acceptTermsCell = `<td class='border px-4 py-2'>${entry.acceptedTermsAndconditions}</td>`;
+        // console.log(`entry.acceptedTermsAndconditions is ${entry.acceptedTermsAndconditions}`)
+        const row = `<tr>${nameCell}${emailCell}${passwordCell}${dobCell}${acceptTermsCell}</tr>`;
         return row;
-    }).join("\n")
+    }).join("\n");
 
-    const table = `<table class="table-auto w-full"><tr>
-    <th class="px-4 py-2">Name</th>
-    <th class="px-4 py-2">Email</th>
-    <th class="px-4 py-2">Password</th>
-    <th class="px-4 py-2">dob</th>
-    <th class="px-4 py-2">Accepted terms?</th>
-    </tr>${tableEntries}</table>`
+    //th means table header or column name
+    const table = `<table class="table-auto w-full">
+        <tr>
+            <th class="px-4 py-2">Name</th> 
+            <th class="px-4 py-2">Email</th>
+            <th class="px-4 py-2">Password</th>
+            <th class="px-4 py-2">Dob</th>
+            <th class="px-4 py-2">Accepted terms?</th>
+        </tr>
+        ${tableEntries}
+    </table>`;
 
     let details = document.getElementById("user-entries");
     details.innerHTML = table;
-}
-userEntries = []
-const saveUserForm = (event)=>{
+};
+
+
+const saveUserForm = (event) => {
     event.preventDefault();
+
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-   
-    const acceptedTermsAndconditions = document.getElementById("acceptTerms").value;
-
     const dob = document.getElementById("dob").value;
+    const acceptedTermsAndconditions = document.getElementById("acceptTerms").checked;
+    console.log(`acceptedTermsAndconditions${acceptedTermsAndconditions}`)
+
     const currentDate = new Date();
     const userDob = new Date(dob);
     const age = currentDate.getFullYear() - userDob.getFullYear();
@@ -63,18 +71,17 @@ const saveUserForm = (event)=>{
         password,
         dob,
         acceptedTermsAndconditions
-    }
-    userEntries.push(entry);
-    let existingEntries = localStorage.getItem("user-entries");
-    if (existingEntries) {
-        existingEntries = JSON.parse(existingEntries);
-        existingEntries.push(...userEntries);
-        localStorage.setItem("user-entries", JSON.stringify(existingEntries));
-    } else {
-        localStorage.setItem("user-entries", JSON.stringify(userEntries));
-    }
+    };
+
+    const entries = retrieveEntries();
+    entries.push(entry);
+    localStorage.setItem("user-entries", JSON.stringify(entries));
+
     displayEntries();
-    
-}
-userForm.addEventListener("submit",saveUserForm);
+};
+
+
+userForm.addEventListener("submit", saveUserForm);
+
+
 displayEntries();
